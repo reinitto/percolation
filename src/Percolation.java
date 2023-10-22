aa78048d0b347ebdf7ab3b8b1266d97e6df35b29
Reinis <reinitto@gmail.com>
2023-10-22 05:50:33 +0200

    fix virtual sites

--- src/Percolation.java
+++ src/Percolation.java
@@ -12,17 +12,16 @@ public class Percolation {
     // creates n-by-n grid, with all sites initially blocked
     // points to itself == blocked
     public Percolation(int n) {
-        // grid[0]==virtual top node
-        // grid[n*n+1]==virtual bottom node
+
         if(n<=0){
             throw new IllegalArgumentException();
         }
-        grid = new int[n*n+2];
-        treeSize = new int[n*n+2];
-        siteIsOpen = new int[n*n+2];
+        grid = new int[n*n];
+        treeSize = new int[n*n];
+        siteIsOpen = new int[n*n];
         N=n;
-        StdOut.println("grid");
-        StdOut.println(Arrays.toString(grid));
+//        StdOut.println("grid");
+//        StdOut.println(Arrays.toString(grid));
 
 
 
@@ -32,43 +31,38 @@ public class Percolation {
             treeSize[i] = 1;
             siteIsOpen[i] = -1;
         }
-        siteIsOpen[0]=1;
-        siteIsOpen[grid.length-1] =1;
-        // set virtual top and bottom sites to be connected to top and bottom layer
-        // first n+1 nodes point to 0
-
-        for(int i=0;i<n+1;i++){
+        // instead of virtual site, connect all top row sites
+        for(int i=0;i<n ;i++){
             grid[i]=0;
         }
-        // virtual bottom node points to
-        // last row of nodes
-        for(int i = n*n+1-n ; i< n*n+2;i++){
-            grid[i]=n*n+1;
+        // last row of nodes points to last node
+        for(int i = n*n-n ; i < grid.length;i++){
+            grid[i]=n*n;
+
         }
     }
 
 
     private  int getPos(int row, int col) {
-        if(row < 0 || row > N+1){
-            StdOut.println(row);
+        if(row < 0 || row > N - 1){
+//            StdOut.println(row);
             throw new IllegalArgumentException("row out of bounds");
         }
-        if(col < 0 || col >  N+1){
-            StdOut.println(col);
+        if(col < 0 || col >  N - 1){
+//            StdOut.println(col);
             throw new IllegalArgumentException("col out of bounds");
 
         }
-
-        return row*N+col+1;
+        return row*N+col;
     }
 
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         int pos = getPos(row,col);
-        boolean isSiteOpen = isOpen(row,col);
-        StdOut.println("isSiteOpen");
-        StdOut.println(isSiteOpen);
+//        boolean isSiteOpen = isOpen(row,col);
+//        StdOut.println("isSiteOpen");
+//        StdOut.println(isSiteOpen);
         if(!isOpen(row,col)){
             grid[pos]=1;
             openSites+=1;
@@ -92,31 +86,29 @@ public class Percolation {
         // get root of current node
         int nodeRoot = getRoot(row,col);
         // get 4-directional sites
-        StdOut.println("connect");
-
-
-        StdOut.println(row);
-        StdOut.println(col);
-        StdOut.println(nodeRoot);
+//        StdOut.println("connect");
+//
+//
+//        StdOut.println(row);
+//        StdOut.println(col);
+//        StdOut.println(nodeRoot);
 
         int leftr = row;
-        int leftc = col == 0? -1 : col-1;
+        int leftc = col == 0 ? -1 : col-1;
         int rightr = row;
-        int rightc = col == N ? -1 : col+1;
+        int rightc = col == N -1 ? -1 : col+1;
         int topr = row == 0 ? -1 : row -1;
         int topc = col;
-        int bottomr = row == N + 2 ? -1 : row + 1;
+        int bottomr = row == N -1 ? -1 : row + 1;
         int bottomc = col;
         int[][]sites = {{leftr, leftc}, {rightr, rightc}, {topr, topc}, {bottomr, bottomc}};
         // if sites are open, connect them
 
-        StdOut.println("sites");
-        for(int g=0; g < sites.length;g++){
-            StdOut.println(Arrays.toString(sites[g]));
-
-        }
+//        StdOut.println("sites");
+//        for(int g=0; g < sites.length;g++){
+//            StdOut.println(Arrays.toString(sites[g]));
+//        }
         for(int i = 0; i< sites.length;i++){
-
             int crow = sites[i][0];
             int ccol = sites[i][1];
             if((crow >= 0 && crow < N) && (ccol > -1 && ccol < N)){
@@ -167,7 +159,7 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return isFull(N+1,0);
+        return isFull(N-1,N-1);
     }
 
 
@@ -176,25 +168,18 @@ public class Percolation {
         int N = Integer.parseInt(args[0]);
         Percolation perk = new Percolation(N);
         // print only grid without virtual nodes
-        for(int a =1; a<perk.grid.length-1 ;a++){
+        for(int a =0; a<perk.grid.length ;a++){
             StdOut.println(perk.grid[a]);
         }
 
         // open some random sites
         for(int i = 0; i < perk.N*2; i++){
-            StdOut.println("opening rand site");
+//            StdOut.println("opening rand site");
             int randRow = StdRandom.uniformInt(0,N);
             int randCol = StdRandom.uniformInt(0,N);
-             StdOut.println(randRow);
-             StdOut.println(randCol);
             perk.open(randRow,randCol);
             StdOut.println(Arrays.toString(perk.grid));
         }
-
-        StdOut.println("final");
-        StdOut.println(Arrays.toString(perk.grid));
-        StdOut.println(perk.isFull(perk.N-1,perk.N-2));
-
 
     }
 }
